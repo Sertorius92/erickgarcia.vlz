0924d1118436af5366f92d672d1512d61deb4776
erickgarcia.vlz <masterofmetal01@hotmail.com>
2023-04-02 20:40:00 -0500

    title update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'Erick Garcia Velez',
   author: 'Tails Azimuth',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
